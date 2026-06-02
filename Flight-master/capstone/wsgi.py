896db4d54f5e8769b8a3a965e391885251b14d9a
KrishParashar37 <krishparashar609@gmail.com>
2026-06-02 22:43:32 +0530

    Fixed startup bugs and added hosting configurations

--- Flight-master/capstone/wsgi.py
+++ Flight-master/capstone/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the directory containing 'capstone' to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
