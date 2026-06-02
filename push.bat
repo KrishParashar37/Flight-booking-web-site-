0711b955c5f189bf78f5a90021b04d06004d500c
KrishParashar37 <krishparashar609@gmail.com>
2026-06-02 20:45:58 +0530

    Fixed startup bugs and added hosting configurations

--- push.bat
+++ push.bat
@@ -7,9 +7,13 @@ git add .
 git commit -m "Fixed startup bugs and added hosting configurations"
 git branch -M main
 
-:: Remote origin add kar rahe hain (agar pehle se ho to ignore error)
+:: Remote origin add kar rahe hain
 git remote remove origin 2>nul
 git remote add origin https://github.com/KrishParashar37/Flight-booking-web-site-.git
+
+echo.
+echo Pehle remote repository se changes pull kar rahe hain...
+git pull origin main --allow-unrelated-histories --no-edit
 
 echo.
 echo Pushing code to GitHub...
